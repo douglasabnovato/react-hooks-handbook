ac441f8e9a3659527a792d581f4a6832ff289505
Douglas A B Novato <douglasabnovato@gmail.com>
2022-09-02 16:43:41 -0300

    componente useRef com duas funcionalidades

--- src/App.js
+++ src/App.js
@@ -9,30 +9,42 @@ import UseContext from "./components/UseContext";
 import UseReducer from "./components/UseReducer";
 import UseCallback from "./components/UseCallback";
 import UseMemo from "./components/UseMemo";
+import UseRef from "./components/UseRef";
 
 export default function App(props) {
-
   return (
     <div className="App">
       <h1>Hooks React</h1>
       <div className="Cards">
-        <Card titulo="#00 - Hooks useState" color="#B9006E">
+        <Card titulo="#00 - Hooks useState" color="#E7E014">
           <UseState />
         </Card>
         <Card titulo="#01 - Components Lifecycle useEffect" color="#AF601A">
           <UseEffect />
         </Card>
-        <Card titulo="#02 - Consumindo Contextos com useContext" color="#168A2B">
+        <Card
+          titulo="#02 - Consumindo Contextos com useContext"
+          color="#168A2B"
+        >
           <UseContext />
         </Card>
-        <Card titulo="#03 - Manipulando múltiplos state ao mesmo tempo com useReducer" color="#0D3BE6">
+        <Card
+          titulo="#03 - Manipulando múltiplos state ao mesmo tempo com useReducer"
+          color="#0D3BE6"
+        >
           <UseReducer />
         </Card>
-        <Card titulo="#04 - Memorizando funções com useCallback" color="#E7E014">
+        <Card
+          titulo="#04 - Memorizando funções com useCallback"
+          color="#B9006E"
+        >
           <UseCallback />
         </Card>
         <Card titulo="#05 - Memorizando dados com useMemo" color="#921E28">
           <UseMemo />
+        </Card>
+        <Card titulo="#06 - Entendendo referências no React" color="#FFC300">
+          <UseRef />
         </Card>
       </div>
       <h5>@douglasabnovato</h5>
--- src/components/UseRef/index.jsx
+++ src/components/UseRef/index.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./index.css";
 
 function UseRef() {
   return (
     <div className="UseRef">
-      <h1>UseRef</h1>
+      <UseRefCurrent />
+      <UseRefInput />
+    </div>
+  );
+}
+
+function UseRefInput() {
+  console.log("rendered UseRefInput");
+  const inputRef = useRef(null)
+
+  function handlePrintName() {
+    alert(inputRef.current.value);
+  }
+
+  return (
+    <div className="UseRefInput">
+      <input ref={inputRef} />
+      <button onClick={handlePrintName}>Print name</button>
+    </div>
+  );
+}
+
+function UseRefCurrent() {
+  console.log("rendered UseRefCurrent");
+  const number = useRef(0);
+
+  function handleSetValue() {
+    const newNumber = Math.round(Math.random() * (10 - 1) * 1);
+    number.current = newNumber;
+  }
+
+  function handlePrintValue() {
+    alert(number.current);
+  }
+
+  return (
+    <div className="UseRefCurrent">
+      <button onClick={handleSetValue}>Set Value</button>
+      <button onClick={handlePrintValue}>Print Value</button>
     </div>
   );
 }
