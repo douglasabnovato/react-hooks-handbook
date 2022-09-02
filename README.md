b93b51a0c9283f9e3a7dee4931e7c34a8032e17c
Douglas A B Novato <douglasabnovato@gmail.com>
2022-09-02 14:01:53 -0300

    componente useReducer

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 - [x] useEffect #01: está executando duas vezes. :. é como as coisas funcionam agora no React, a única maneira de desabilitar esse comportamento é desabilitar o modo estrito. Em create-react-app você pode ir no seu arquivo index.js e remover o componente de ordem superior StrictMode.
 - [x] useContext #02: consumir contextos 
 - [ ] useReducer #03: manipular múltiplos state ao mesmo tempo 
+
+### Referências
+
+Conteúdo do Mateus Silva
+- [Instagram](https://www.instagram.com/imateus.silva/)
+- [e-book React Hooks](https://devacademy.com.br/ebooks/react-hooks/)
+- [playlist React Hooks](https://www.youtube.com/playlist?list=PLPXWI3llyMiLayGs4_noY8Q1-rbmbFoYM)
--- src/components/UseReducer/index.jsx
+++ src/components/UseReducer/index.jsx
@@ -1,12 +1,47 @@
-import React, { createContext } from 'react'
-import './index.css'
+import React, { useReducer } from "react";
+import "./index.css";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "plus":
+      return {
+        counter: state.counter + 1,
+        clicks: state.clicks + 1,
+      };
+    case "minus":
+      return {
+        counter: state.counter - 1,
+        clicks: state.clicks + 1,
+      };
+    default:
+        return state;
+  }
+}
+
+const initialValue = {
+    counter: 0,
+    clicks: 0,
+}
 
 function UseReducer() {
-    return (
-        <div>
-            <h2>UseReducer</h2>
-        </div>
-    )
+
+    const [state, dispatch] = useReducer(reducer, initialValue)
+
+    function handlePlus(){
+        dispatch({ type: 'plus'});
+    }
+
+    function handleMinus(){
+        dispatch({ type: 'minus'});
+    }
+  return (
+    <div>
+      <h1>{state.counter}</h1>
+      <h4>Cliques: {state.clicks}</h4>
+      <button onClick={handleMinus}>-</button>
+      <button onClick={handlePlus}>+</button>
+    </div>
+  );
 }
 
 export default UseReducer;
