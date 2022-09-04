04208a4f0048683ae9cd1c0c4572d6da921f8ac7
Douglas A B Novato <douglasabnovato@gmail.com>
2022-09-04 13:49:48 -0300

    componente useCustom construído com useFetch

--- src/components/UseCustom/index.jsx
+++ src/components/UseCustom/index.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import "./index.css";
 
 function UseCustom() {
+  const [loading, response] = useFetch('https://api.github.com/users/maateusilva')
+
+  if(loading){
+    return <h1>Loading ...</h1>
+  }
 
   return (
     <div className="UseCustom">
-      <h2>UseCustom</h2> 
+      {JSON.stringify(response)}
     </div>
   );
 }
 
+function useFetch(url){
+  const [loading, setLoading] = useState(true)
+  const [response, setResponse] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch(url)
+      const json = await resp.json()
+
+      setLoading(false)
+      setResponse(json)
+    })()
+  }, [url])
+
+  return [loading, response]
+}
 
 export default UseCustom;
